49662f93b293904b4f43ea723e598b1dfdb70ec5
GameCobra <adrian@houser.ca>
2025-02-14 20:03:00 -0500

    Fixed circular import

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from TurtleDrawer import *
 
 def Start():
-    DrawQuad(0, 0, 100, 100, RGB(100, 0, 0), True)
+    import TurtleDrawer as TD
+    TD.DrawQuad(0, 0, 100, 100, TD.RGB(100, 0, 0), True)
 
 def Update():
+    import TurtleDrawer as TD
+    TD.teleport(0, 0)
     pass
