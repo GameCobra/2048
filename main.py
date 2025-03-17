db64bba1d4ae02795970c5ee272b8ac44f096ed4
GameCobra <adrian@houser.ca>
2025-03-17 11:44:23 -0400

    Started work on animations

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import TurtleDrawer
 import random
 import keyboard
 import math
-GameGrid = [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]]
+import copy
+StartGrid = [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]]
+GameGrid = copy.deepcopy(StartGrid)
+previousGrid = []
+lastActionFrame = 0
 
 def Start():
     import TurtleDrawer as TD
     import Tools
-    TD.SetFramerate(-1)
+    TD.SetFramerate(10)
     Tools.DefineScreenSize(500, 500)            
     keyboard.on_press(on_key_press)
     #print(Tools.BezierCurve3p(0,0, 0, 100, 100, 0))
@@ -15,14 +19,20 @@ def Start():
 
 
 def Update(frame : int):
+    global lastActionFrame
     import TurtleDrawer as TD
     import Tools
     TD.ClearScreen()
+    if lastActionFrame == -1:
+        lastActionFrame = frame
     SquareRadius = 125
     grid = Tools.MakeGrid(-500 + SquareRadius, -500 + SquareRadius, 500 - SquareRadius, 500 - SquareRadius, 4, 4, True)
     for item in grid:
         TD.DrawQuad(item[0]- SquareRadius, item[1] - SquareRadius, item[0] + SquareRadius, item[1] + SquareRadius, TD.RGB(155 , 135, 117), TD.RGB(189, 172, 151), 20)
     for item in grid:
+        if previousGrid != [] and  GameGrid[item[2]][item[3]] == previousGrid[item[2]][item[3]]:
+            item[0] += 25
+            item[1] += 25
         if GameGrid[item[2]][item[3]] != "":
             TD.DrawQuad(item[0]- SquareRadius, item[1] - SquareRadius, item[0] + SquareRadius, item[1] + SquareRadius, TD.RGB(155 , 135, 117), TD.RGB(241, 174, 114), 20)
         TD.WriteText(GameGrid[item[2]][item[3]], item[0], item[1], 65, VAlign="center",  color=[1, 1, 1])
@@ -31,23 +41,42 @@ def Update(frame : int):
         #TD.WriteText()
 
 def on_key_press(event):
+    previousGrid = copy.deepcopy(GameGrid)
+    print(previousGrid)
+    global lastActionFrame
+    beforeGrid = copy.deepcopy(GameGrid)
     #print(f"Key {event.name} pressed")
     if event.name == "s":
         for i in range(len(GameGrid)):
             GameGrid[i] = MergeStrip(GameGrid[i])
+        if beforeGrid == GameGrid and GameGrid != StartGrid:
+            return
         GenerateTile()
+
     if event.name == "w":
         for i in range(len(GameGrid)):
             GameGrid[i] = list(reversed(MergeStrip(list(reversed(GameGrid[i])))))
+        if beforeGrid == GameGrid and GameGrid != StartGrid:
+            return
         GenerateTile()
+
     if event.name == "a":
         for i in range(len(GameGrid)):
             SetHorazontalSlice(i, MergeStrip(GetHorazontalSlice(i)))
+        if beforeGrid == GameGrid and GameGrid != StartGrid:
+            return
         GenerateTile()
+
     if event.name == "d":
         for i in range(len(GameGrid)):
             SetHorazontalSlice(i, list(reversed(MergeStrip(list(reversed(GetHorazontalSlice(i)))))))
+        if beforeGrid == GameGrid and GameGrid != StartGrid:
+            return
         GenerateTile()
+
+    lastActionFrame = -1
+    
+
     
         
 
